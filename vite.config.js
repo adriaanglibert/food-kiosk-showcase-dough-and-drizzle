e8ab374b1b648eae723db24b4920384927916598
adriaan-glibert-development <info@adriaanglibert.be>
2025-06-20 17:02:34 +0200

    Build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const basePath = process.env.VITE_BASE_PATH || '/';
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  const basePath = env.VITE_BASE_PATH || '/';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: basePath
-})
+  console.log(basePath);
+
+  return {
+    plugins: [react()],
+    base: basePath
+  };
+});
